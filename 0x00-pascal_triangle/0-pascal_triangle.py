680ed837c479b79791d15a904ad8e5876513e2fe
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2024-10-03 14:17:51 +0100

    Create File 0-pascl_triangle.py

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" Create Pascale Triangle"""
+
+def pascal_triangle(n):
+    if (n <= 0):
+        return None
+    else:
+        res = []
+        for i in range(1, n + 1):
+            item = []
+            C = 1
+            for j in range (1, i + 1):
+                item.append(C)
+                C = C * (i - j) // j
+            res.append(item)
+    return res
