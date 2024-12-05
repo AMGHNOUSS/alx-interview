1690802c0e25303a4165568a4a87d52f6ee465f0
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2024-12-05 19:16:51 +0100

    Fix error

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,24 +1,26 @@
 #!/usr/bin/python3
-""" Island Perimeter """
+"""Defines island perimeter finding function."""
 
 
 def island_perimeter(grid):
-    line = len(grid)
-    row = len(grid[0])
-    sum = 0
-    check = 0
+    """Return the perimiter of an island.
+    The grid represents water by 0 and land by 1.
+    Args:
+        grid (list): A list of list of integers representing an island.
+    Returns:
+        The perimeter of the island defined in grid.
+    """
+    width = len(grid[0])
+    height = len(grid)
+    edges = 0
+    size = 0
 
-    for i in range(line):
-        for j in range(row):
-            check = 0
-            if (grid[i][j] == 1):
-                if (j < row and grid[i][j + 1] == 0):
-                    check += 1
-                if (j > -1 and grid[i][j - 1] == 0):
-                    check += 1
-                if (i < line and grid[i + 1][j] == 0):
-                    check += 1
-                if (i > -1 and grid[i - 1][j] == 0):
-                    check += 1
-            sum += check
-    return (sum)
+    for i in range(height):
+        for j in range(width):
+            if grid[i][j] == 1:
+                size += 1
+                if (j > 0 and grid[i][j - 1] == 1):
+                    edges += 1
+                if (i > 0 and grid[i - 1][j] == 1):
+                    edges += 1
+    return size * 4 - edges * 2
