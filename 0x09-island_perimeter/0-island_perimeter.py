37d043fb6c9cbb69b46cd5a9ebb2ae9ca7de8421
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2024-12-05 19:28:28 +0100

    Fix error

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -4,22 +4,19 @@
 
 def island_perimeter(grid):
     """Return the perimiter of an island."""
-    line = len(grid)
-    row = len(grid[0])
+    lines = len(grid)
+    rows = len(grid[0])
     sum = 0
-    check = 0
 
-    for i in range(line):
-        for j in range(row):
+    for i in range(lines):
+        for j in range(rows):
             if (grid[i][j] == 1):
-                if (j < row and grid[i][j + 1] == 0):
-                    check += 1
-                if (j > -1 and grid[i][j - 1] == 0):
-                    check += 1
-                if (i < line and grid[i + 1][j] == 0):
-                    check += 1
-                if (i > -1 and grid[i - 1][j] == 0):
-                    check += 1
-                sum += check
-                check = 0
+                if (i == lines - 1 or grid[i + 1][j] == 0):
+                    sum += 1
+                if (i == 0 or grid[i - 1][j] == 0):
+                    sum += 1
+                if (j == rows - 1 or grid[i][j + 1] == 0):
+                    sum += 1
+                if (j == 0 or grid[i][j - 1] == 0):
+                    sum += 1
     return (sum)
