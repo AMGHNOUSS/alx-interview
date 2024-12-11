1e621b0197045e36c8b9b46e3dfae6068c85c125
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2024-12-11 23:46:22 +0100

    Finish

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -3,21 +3,40 @@
 
 
 def isWinner(x, nums):
-
-    lenght = len(nums)
-    numbers_prime = 0
-    index = 0
-    for i in range(lenght):
-        index = 0
-        for j in range(2, nums[i]):
-            if (nums[i] % j == 0):
-                index += 1
-        if (index == 0):
-            numbers_prime += 1
-    x -= numbers_prime
-    if (x <= 0):
+    """Determines the winner of the prime game."""
+    if x < 1 or not nums:
         return None
-    elif (x % 2 == 0):
+
+    # Precompute prime numbers up to the maximum value in nums using the Sieve of Eratosthenes
+    max_n = max(nums)
+    is_prime = [True] * (max_n + 1)
+    is_prime[0] = is_prime[1] = False  # 0 and 1 are not primes
+
+    for i in range(2, int(max_n ** 0.5) + 1):
+        if is_prime[i]:
+            for j in range(i * i, max_n + 1, i):
+                is_prime[j] = False
+
+    # Precompute the cumulative count of primes up to each number
+    prime_count = [0] * (max_n + 1)
+    for i in range(1, max_n + 1):
+        prime_count[i] = prime_count[i - 1] + (1 if is_prime[i] else 0)
+
+    # Determine the winner for each round
+    maria_wins = 0
+    ben_wins = 0
+
+    for n in nums:
+        # If the count of primes is odd, Maria wins; otherwise, Ben wins
+        if prime_count[n] % 2 == 1:
+            maria_wins += 1
+        else:
+            ben_wins += 1
+
+    # Determine the overall winner
+    if maria_wins > ben_wins:
         return "Maria"
-    else:
+    elif ben_wins > maria_wins:
         return "Ben"
+    else:
+        return None
